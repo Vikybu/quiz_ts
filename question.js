cb6ccc605a5f10dd5c359632bc5e762ea930f7c4
Sj88 <Stanjamme@live.fr>
2025-06-25 10:51:51 +0200

    Fonction Content, NextQuestion, ClearQuestion

--- question.js
+++ question.js
@@ -1,5 +1,5 @@
-export const quizzic = {
-    questions: [
+export const quizzic = 
+     [
         {
             text: "C'est ki ki é la reine du game de la zik ?",
             options: [
@@ -41,5 +41,5 @@ export const quizzic = {
             ],
             correct_answer: "L'irlande"
         }
-    ]
-}
+]
+
