e1dcad588d5f974ad6b5c8b323c74b5d7097abfc
Sj88 <Stanjamme@live.fr>
2025-06-26 11:38:10 +0200

    Ajout fichier EndOfThegame

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-<<<<<<< branch_ju
-import {content, nextQuestion, clearQuestion} from './game.js'
+import {content, nextQuestion, clearQuestion,buttonOff} from './game.js'
 import {answer, scoreCount, answerStyle, refreshAddEventListener} from './script.js'
-=======
-import {content, nextQuestion, clearQuestion, buttonOff} from './game.js'
-import {answer, scoreCount, refreshAddEventListener} from './script.js'
->>>>>>> main
+
 
 let button = document.querySelector(".btn_suivant")
 
