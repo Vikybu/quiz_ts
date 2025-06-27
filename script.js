3bed43a83fcfc84b158fcf8a65436d03e4560c23
Vikybu <ferryvictoria@hotmail.fr>
2025-06-27 11:35:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-import {disabledAnswer,buttonActivation} from './game.js'
+import { disabledAnswer, buttonActivation, content } from './game.js'
 
 let score = 0
-const btnAnswer = document.querySelectorAll('.btn_answer')
-let button = document.querySelector(".btn_suivant")
-
 
 function answer(clickBtn) {
-const btnAnswer = document.querySelectorAll('.btn_answer')
     if (clickBtn.target.getAttribute("data-id") === "true") {
         console.log("true")
         return true
@@ -24,35 +20,26 @@ function scoreCount(clickBtn) {
     }
 }
 
-
-function answerStyle(clickBtn) {
-   
-    if (clickBtn.target.getAttribute("data-id") === "true") { 
-        
-        clickBtn.target.style.border = "2px solid green"
-        clickBtn.target.style.backgroundColor = "#e8f5e8"
-    } else {
-        
-        clickBtn.target.style.border = "2px solid red"
-        clickBtn.target.style.backgroundColor = "#ffe8e8"
-    }
-}
-
-
-
-
-
-function refreshAddEventListener() {
-     const btnAnswer = document.querySelectorAll('.btn_answer')
-    for (let i = 0; i < btnAnswer.length; i++) {
-    btnAnswer[i].addEventListener("click", (click) => {
-        answer(click)
-        scoreCount(click) 
-        answerStyle(click)  
-        disabledAnswer()
-        buttonActivation()
-} )
+function scoreDisplay() {
     
+if (score <= 1){
+    const messageScore = document.createElement('p')
+    messageScore.innerText = "oh no"
+    document.body.appendChild(messageScore)
+      console.log('Essai encore')
+    return "oh no"
+} if (score <= 3) {
+    const messageScore = document.createElement('p')
+    messageScore.innerText = "Tu vas y arriver !!!"
+    document.body.appendChild(messageScore)
+    console.log("Presque")
+    return "Tu vas y arriver !!!"
+} else if (score == 4) {
+    const messageScore = document.createElement('p')
+    messageScore.innerText = "Bravo !!!"
+    document.body.appendChild(messageScore)
+    console.log("Good job")
+    return "Bravo !!!"
 }
 }
 
@@ -61,19 +48,32 @@ function resetScore() {
     score = 0
 }
 
-export {answer, scoreCount, answerStyle, refreshAddEventListener}
 
+function answerStyle(clickBtn) {
+    if (clickBtn.target.getAttribute("data-id") === "true") {
 
+        clickBtn.target.style.border = "2px solid green"
+        clickBtn.target.style.backgroundColor = "#E8F5E8"
+    } else {
 
-
-function scoreDisplay(params) {
-
+        clickBtn.target.style.border = "2px solid red"
+        clickBtn.target.style.backgroundColor = "#FFE8E8"
+    }
 }
 
-function rematch(params) {
 
+function refreshAddEventListener() {
+    const btnAnswer = document.querySelectorAll('.btn_answer')
+    for (let i = 0; i < btnAnswer.length; i++) {
+        btnAnswer[i].addEventListener("click", (click) => {
+            answer(click)
+            scoreCount(click)
+            answerStyle(click)
+            disabledAnswer()
+            buttonActivation()
+           
+        })
+    }
 }
 
-function reset(params) {
-
-}
+export { answer, scoreCount, answerStyle, refreshAddEventListener, resetScore, scoreDisplay }
